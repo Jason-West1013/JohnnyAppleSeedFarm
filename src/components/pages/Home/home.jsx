0b1f838d18382240396447bd1b8a79053d6df7c4
Jason West <jason@Jasons-MacBook-Pro.local>
2018-05-31 08:52:52 -0400

    added images, continuing work on the homepage.

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -1,62 +1,84 @@
 import React, { Component } from 'react';
-import Media from 'react-media';
 
 // styles 
 import { 
-    HomeContainer, 
-    Welcome, 
-    Message,
-    RightImage,
-    GreetingContainer
-} from './home_style'
+    HomeContainer,
+    SignContainer,
+    MessageHeader,
+    TopSection,
+    BottomSection,
+    Signature,
+    MiddleSection,
+    MidTop,
+    MidTopLeftImage,
+    MidTopOldOrchardMessage,
+    MidTopRightImage,
+    MiddleMiddle,
+    MiddleBottom
+} from './home_style';
 
 // images
-import orchard from '../../../images/orchard.jpg';
+import ellingtonSign from '../../../images/ellington_sign_2.JPG';
+import beeGirl from '../../../images/bee_girl.png';
+import appleTree from '../../../images/apple_tree.jpg';
+import eatingApple from '../../../images/eating_apple_2.jpg';
 
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = { showImage: false };
+        this.state = {
+            showImageOnLoad: false
+        };
         this.imageHandler = this.imageHandler.bind(this);
+        this.pageLoad = this.pageLoad.bind(this);
     }
 
-    componentDidMount() { window.addEventListener('scroll', this.imageHandler); }
+    componentDidMount() { window.addEventListener('scroll', this.imageHandler); window.addEventListener('load', this.pageLoad);
+    }
     componentWillUnmount() { window.removeEventListener('scroll', this.imageHandler); }
 
     imageHandler() {
         let currentPosition = window.scrollY;
-        if (currentPosition >= 50) {
-            this.setState({ showImage: true })
+        if (currentPosition >= 0) {
+            this.setState({ showImageOnLoad: true });
         }
     }
 
+    pageLoad() { this.setState({showImageOnLoad: true}); }
+
     render() {
-        const { showImage } = this.state;
+        const { showImageOnLoad } = this.state;
         return(
           <HomeContainer>
-            <GreetingContainer
-              unmountOnExit
-              in={showImage}
-              timeout={1000}
-            >
-              <Media query={{ maxWidth: 1160 }}>
-                {matches => 
-                matches? (
-                  <Welcome>Welcome to our Farm!</Welcome>
-                ) : (
-                  <Welcome>Welcome to Johnny AppleSeed Farm!</Welcome>
-                )
-              }
-              </Media>
-              <Message>
-                The orchards were originally planted in the 1977-78 growing season. Since then we have replaced many of the older trees and built a whole new orchard. The new location, on Schoolhouse Road in Ellington, was started in 1996 and is still being added to. We now offer a total of 60 acres of dwarf apple trees bearing 14 different varieties and 35 acres of easy to reach peach trees bearing 11 different varieties. We also grow many of our own vegetables including tomatoes, winter squash, sweet corn, and tons of pumpkins, to name a few.<br /><br />
-                Fruitfully,<br />
+            <TopSection>
+              <MessageHeader unmountOnExit in={showImageOnLoad} timeout={1000}>
+                We here at the farm are very proud of our beautiful surroundings and the work it has taken to get us here. We would like to tell you a little about the farm and what we have to offer...
+              </MessageHeader>
+            </TopSection>
+            <SignContainer unmountOnExit in={showImageOnLoad} timeout={1000}>
+              <img src={ellingtonSign} alt='Ellington Sign' style={{height: '100%', width: '100%'}} />
+            </SignContainer>
+            <MiddleSection>
+              <MidTop>
+                <MidTopLeftImage src={appleTree} alt='Apple Tree' />
+                <MidTopOldOrchardMessage>
+                  <p>
+                    The orchard at 231 Somers Road (Route 83) in Ellington was originally planted in 1977-78. Since then, we have replaced many of the older trees with new ones to keep our orchards in good production.
+                  </p>
+                </MidTopOldOrchardMessage>
+                <MidTopRightImage src={eatingApple} alt='Eating Apple' />
+              </MidTop>
+              <MiddleMiddle />
+              <MiddleBottom />
+            </MiddleSection>
+            <BottomSection>
+              <img id='beeGirl' src={beeGirl} alt='Bee Girl in Field' />
+              <Signature>
+                Fruitfully, 
+                <br />
                 Kenny and Kim Shores
-              </Message>
-            </GreetingContainer>
-            <RightImage unmountOnExit in={showImage} timeout={1000}>
-              <img src={orchard} alt="Ellington Stand Sign" style={{ height: '100%',width: '100%' }} />
-            </RightImage>
+              </Signature>
+            </BottomSection>
           </HomeContainer>
         )
     }
--- src/components/pages/Home/home_style.js
+++ src/components/pages/Home/home_style.js
@@ -2,70 +2,124 @@ import styled from 'styled-components';
 import transition from 'styled-transition-group';
 
 export const HomeContainer = styled.div `
-    //border: 1px solid red;
     position: relative;
-    height: 700px;
+    height: 2100px;
     width: 100%;
-    background: linear-gradient(90deg, #fbfffc, #c8e6c9 , #fbfffc )
+    background: linear-gradient(90deg, #fbfffc, #c8e6c9 , #fbfffc);
 `;
 
-export const GreetingContainer =  transition.div `
+export const TopSection = styled.div `
     position: relative;
-    z-index: 2;
-    top: 40%;
-    left: 60%;
-    width: 35%;
-    background: #9e9e9e;
+    top: 5%;
+    height: 300px;
+    width: 100%;
+    background: #338a3e;
+`;
+
+export const MessageHeader = transition.p `
+    position: relative;
+    float: right;
+    top: 18%;
+    right: 5%;
+    width 40%;
+    color: #f3f3f5;
+    font-size: 25px;
+    font-family: 'Khula', sans-serif;
+
+    &:enter { opacity: 0.01; }
+
+    &:enter-active {
+        opacity: 1; 
+        transition: all 2000ms ease;
+    }
+`;
+
+export const SignContainer = transition.div `
+    position: absolute;
+    top: 2.5%;
+    left: 2%;
+    width: 50%;
+    height: 20%;
 
     &:enter {
         opacity: 0.01;
-        transform: translateX(50%);
-    }
-
-    &:enter-active {
-        opacity: 1;
-        transform: translateX(0%);
-        transition: all 800ms ease-out;
-    }
-`
-
-// green bar that says welcome
-export const Welcome = styled.div `
-    position: absolute;
-    float: left;
-    top: -30px;
-    right: 30%;
-    height: 40px;
-    width: 75%;
-    text-align: center;
-    border-top: 10px solid #338a3e;
-    background: #66bb6a;
-    font-family: 'Kalam', cursive;
-    font-size: 18px;
-`;
-
-export const Message = styled.p `
-    border: 5px solid #cfcfcf;
-    padding: 5% 8%;
-    text-indent: 30px;
-`;
-
-export const RightImage = transition.div `
-    position: absolute;
-    top: 4%;
-    left: 2%;
-    width: 60%;
-    height: 60%;
-    //border: 1px solid red;
-
-    &:enter { 
-        opacity: 0.01; 
         transform: translateX(-50%);
     }
 
     &:enter-active {
         opacity: 1;
         transform: translateX(0%);
-        transition: all 800ms ease-out;
+        transition: all 1000ms ease;
     }
+`;
+
+export const MiddleSection = styled.div `
+    position: relative;
+    top: 5%;
+    height: 1100px;
+    width: 100%;
+`;
+
+export const MidTop = styled.div `
+    border: 1px solid black;
+    width: 100%;
+    height: 400px;
+    text-align: center;
+`;
+
+export const MidTopLeftImage = styled.img `
+    position: relative;
+    float: left;
+    width: 100px;
+    height: 100px;
+`;
+
+export const MidTopOldOrchardMessage = styled.div `
+    border: 1px solid black;
+    display: inline-block;
+    vertical-align: middle;
+    height: 100%;
+    width: 50%;
+    text-align: center;
+`;
+
+export const MidTopRightImage = styled.img `
+    position: relative;
+    float: right;
+    transform: rotate(-10deg);
+    width: 100px;
+    height: 100px;
+`;
+
+export const MiddleMiddle = styled.div `
+    border: 1px solid red;
+    width: 100%;
+    height: 100px;
+`;
+
+export const MiddleBottom = styled.div `
+    border: 1px solid black;
+    width: 100%;
+    height: 100px;
+`;
+
+// Bottom section of homepage
+export const BottomSection = styled.div `
+    position: absolute;
+    bottom: 1%;
+
+    #beeGirl {
+        position: relative;
+        height: 100%;
+        width: 100%;
+    }
+`;
+
+export const Signature = styled.p `
+    position: absolute;
+    bottom: 15%;
+    left: 5%;    
+    color: #f3f3f5;
+    font-size: 46px;
+    font-family: 'Sacramento', cursive;
 `;
